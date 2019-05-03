9b82c35ca98af8007caf308fdbcfc3da68ec1082
Delfeil <delfeilcasanova@gmail.com>
2019-05-03 21:41:35 +0200

    gestion routes correspondances

--- AccidentologyService/routes/index.js
+++ AccidentologyService/routes/index.js
@@ -1,9 +1,23 @@
+var fs = require('fs');
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Projet tutoré Accidentologie' });
+  // res.render('index', { title: 'Projet tutoré Accidentologie' });
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/javascripts/correspondance.js', function(req, res){
+  res.sendFile(path.join(__dirname + '/public/javascripts/correspondance.js'));
+});
+
+// route correspondance code api -> code bdd
+router.get('/p?id/correspondance', function(req, res) {
+  var correspondances = JSON.parse(fs.readFileSync('Association_code_OWM_DB.json'));
+  var apiId = req.route.query.id;
+  var bddLabel = correspondances[apiId]["label-bdac"];
+  res.send(bddLabel);
 });
 
 module.exports = router;
